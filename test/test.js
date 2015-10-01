25314b80dc586d94fe4dfeaa51ca41e973c44ec9
Yannick Schurter <yannick.schurter@gmail.com>
2015-10-01 13:27:45 +0200

    test history

--- test/test.js
+++ test/test.js
@@ -2,7 +2,6 @@
 "use strict";
 
 process.env.NODE_ENV = 'production';
-process.env.BYPASS   = 'true';
 
 var request = require('supertest');
 var expect  = require('chai').expect;
@@ -11,15 +10,19 @@ var config  = require('../lib/config.js');
 var mongo   = require('../lib/mongo.js');
 var app;
 
-var mongoUrl = 'mongodb://' + config.MONGO.ADDRESS + ':' + config.MONGO.PORT + '/test';
+config.BYPASS = true;
+config.HISTORY.DEBOUNCE_TIME = 0;
+
+var mongoUrl = `mongodb://${config.MONGO.ADDRESS}:${config.MONGO.PORT}/test`;
 
 var cardID = 'sd123';
 var analysisID;
+var historyID;
 
 /**
  * Tests a complete scenario involving sequences of requests
  */
-describe("Routes", function () {
+describe('Routes', function () {
   before(function (done) {
     mongo.connect(mongoUrl, function (err) {
       if (err) { throw err; }
@@ -29,23 +32,23 @@ describe("Routes", function () {
     });
   });
 
-  it('GET /api/platforms/' + cardID, function (done) {
+  it(`GET    /api/platforms/${cardID}`, function (done) {
     request(app)
-    .get('/api/platforms/' + cardID)
+    .get(`/api/platforms/${cardID}`)
     .expect(404)
     .end(done);
   });
 
-  it('GET /api/platforms/' + cardID + '/analyses', function (done) {
+  it(`GET    /api/platforms/${cardID}/analyses`, function (done) {
     request(app)
-    .get('/api/platforms/' + cardID + '/analyses')
+    .get(`/api/platforms/${cardID}/analyses`)
     .expect(404)
     .end(done);
   });
 
-  it('POST /api/platforms/' + cardID + '/analyses', function (done) {
+  it(`POST   /api/platforms/${cardID}/analyses`, function (done) {
     request(app)
-    .post('/api/platforms/' + cardID + '/analyses')
+    .post(`/api/platforms/${cardID}/analyses`)
     .send({ foo: 'bar' })
     .expect('Content-Type', /json/)
     .expect(201)
@@ -78,11 +81,11 @@ describe("Routes", function () {
     });
   });
 
-  it("PUT /api/platforms/" + cardID + "/analyses/:id", function (done) {
+  it(`PUT    /api/platforms/${cardID}/analyses/:id`, function (done) {
     expect(analysisID).to.exist;
 
     request(app)
-    .put('/api/platforms/' + cardID + '/analyses/' + analysisID)
+    .put(`/api/platforms/${cardID}/analyses/${analysisID}`)
     .send({ bar: 'foo' })
     .expect('Content-Type', /json/)
     .expect(200)
@@ -117,9 +120,9 @@ describe("Routes", function () {
     });
   });
 
-  it("GET /api/platforms/" + cardID, function (done) {
+  it(`GET    /api/platforms/${cardID}`, function (done) {
     request(app)
-    .get('/api/platforms/' + cardID)
+    .get(`/api/platforms/${cardID}`)
     .expect('Content-Type', /json/)
     .expect(200)
     .expect(function (res) {
@@ -133,9 +136,9 @@ describe("Routes", function () {
     .end(done);
   });
 
-  it('GET /api/platforms/' + cardID + '/analyses', function (done) {
+  it(`GET    /api/platforms/${cardID}/analyses`, function (done) {
     request(app)
-    .get('/api/platforms/' + cardID + '/analyses')
+    .get(`/api/platforms/${cardID}/analyses`)
     .expect('Content-Type', /json/)
     .expect(200)
     .expect(function (res) {
@@ -147,11 +150,11 @@ describe("Routes", function () {
     .end(done);
   });
 
-  it("DELETE /api/platforms/" + cardID + "/analyses/:id", function (done) {
+  it(`DELETE /api/platforms/${cardID}/analyses/:id`, function (done) {
     expect(analysisID).to.exist;
 
     request(app)
-    .delete('/api/platforms/' + cardID + '/analyses/' + analysisID)
+    .delete(`/api/platforms/${cardID}/analyses/${analysisID}`)
     .expect(204)
     .end(function (error) {
       expect(error).to.not.exist;
@@ -167,9 +170,85 @@ describe("Routes", function () {
     });
   });
 
-  it("DELETE /api/platforms/" + cardID, function (done) {
+  it(`GET    /api/platforms/${cardID}/history`, function (done) {
     request(app)
-    .delete('/api/platforms/' + cardID)
+    .get(`/api/platforms/${cardID}/history`)
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .expect(function (res) {
+      var body = res.body;
+
+      expect(body).to.be.an('array').with.length(2);
+      expect(body[0]).to.have.property('analyses').that.is.an('array').with.length(1);
+      expect(body[1]).to.have.property('analyses').that.is.an('array').with.length(1);
+
+      expect(body[0]).to.have.property('date');
+      expect(body[1]).to.have.property('date');
+
+      var date1 = new Date(body[0].date);
+      var date2 = new Date(body[1].date);
+      expect(date1.getTime()).to.be.above(date2.getTime());
+
+      expect(body[1].analyses[0]).to.have.property('foo', 'bar'); // was inserted with foo:bar
+      expect(body[0].analyses[0]).to.have.property('bar', 'foo'); // then updated with bar:foo
+
+      historyID = body[1].id;
+    })
+    .end(done);
+  });
+
+  it(`POST   /api/platforms/${cardID}/history/:id`, function (done) {
+    request(app)
+    .post(`/api/platforms/${cardID}/history/${historyID}`)
+    .expect(204)
+    .end(function (error) {
+      expect(error).to.not.exist;
+
+      mongo.get('platforms').findOne({ cardID: cardID }, function (err, doc) {
+        if (err) { throw err; }
+
+        expect(doc).to.have.property('analyses').that.is.an('array').with.length(1);
+        expect(doc).to.have.property('history').that.is.an('array').with.length(3);
+
+        expect(doc.analyses[0]).to.have.property('foo', 'bar');
+
+        expect(doc.history[0]).to.have.property('analyses').that.is.an('array').with.length(0);
+        expect(doc.history[1]).to.have.property('analyses').that.is.an('array').with.length(1);
+        expect(doc.history[2]).to.have.property('analyses').that.is.an('array').with.length(1);
+
+        expect(doc.history[1].analyses[0]).to.have.property('bar', 'foo');
+        expect(doc.history[2].analyses[0]).to.have.property('foo', 'bar');
+
+        done();
+      });
+    });
+  });
+
+  it(`DELETE /api/platforms/${cardID}/history/:id`, function (done) {
+    request(app)
+    .delete(`/api/platforms/${cardID}/history/${historyID}`)
+    .expect(204)
+    .end(function (error) {
+      expect(error).to.not.exist;
+
+      mongo.get('platforms').findOne({ cardID: cardID }, function (err, doc) {
+        if (err) { throw err; }
+
+        expect(doc).to.have.property('history').that.is.an('array').with.length(2);
+
+        expect(doc.history[0]).to.have.property('analyses').that.is.an('array').with.length(0);
+        expect(doc.history[1]).to.have.property('analyses').that.is.an('array').with.length(1);
+
+        expect(doc.history[1].analyses[0]).to.have.property('bar', 'foo');
+
+        done();
+      });
+    });
+  });
+
+  it(`DELETE /api/platforms/${cardID}`, function (done) {
+    request(app)
+    .delete(`/api/platforms/${cardID}`)
     .expect(204)
     .end(function (error) {
       expect(error).to.not.exist;
