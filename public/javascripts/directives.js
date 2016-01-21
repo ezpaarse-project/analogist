592d6f27de5e2f9dbdf04e4200b99cda80ddfc4e
Yannick Schurter <yannick.schurter@gmail.com>
2016-01-21 15:23:38 +0100

    show avatar in the toolbar

--- public/javascripts/directives.js
+++ public/javascripts/directives.js
@@ -6,4 +6,12 @@ angular.module('WebApp')
     scope: { analyses: '=ezAnalyses', authorized: '=ezAuthorized' },
     controller: 'AnalyzerCtrl'
   };
+})
+.directive('ezAvatar', function () {
+  return {
+    restrict: 'E',
+    scope: { user: '=avatarUser' },
+    template: '<span ng-hide="::user.avatarHash" title="{{ ::user.fullName }} (@{{ ::user.username }})" class="initials">{{ ::user.initials }}</span>'
+            + '<img ng-show="::user.avatarHash" title="{{ ::user.fullName }} (@{{ ::user.username }})" ng-src="https://trello-avatars.s3.amazonaws.com/{{ ::user.avatarHash }}/{{ ::(size == \'small\' ? 30 : 50) }}.png">'
+  };
 });
