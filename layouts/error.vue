dda24e04af7ef5081158507fe88e98fa6cfc4a94
Yannick Schurter <yannick.schurter@gmail.com>
2017-09-05 14:34:07 +0200

    add layout to error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,11 +1,30 @@
+
 <template>
-  <section class="text-xs-center">
-    <h1>{{ error.statusCode }}</h1>
-    <h2>{{ error.message }}</h2>
-  </section>
+  <v-app id="analogist" toolbar>
+    <AppDrawer/>
+    <AppHeader/>
+    <main>
+      <v-container fluid>
+        <v-card>
+          <v-card-text class="text-xs-center">
+            <h1>{{ error.statusCode }}</h1>
+            <h2>{{ error.message }}</h2>
+          </v-card-text>
+        </v-card>
+      </v-container>
+    </main>
+  </v-app>
 </template>
+
 <script>
+import AppHeader from '~/components/Header.vue'
+import AppDrawer from '~/components/Drawer.vue'
+
 export default {
-  props: ['error']
+  props: ['error'],
+  components: {
+    AppHeader,
+    AppDrawer
+  }
 }
 </script>
