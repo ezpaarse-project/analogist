58a73ad842874f2b3a0286f949cda021f76e9816
Yannick Schurter <yannick.schurter@gmail.com>
2018-07-30 16:04:10 +0200

    add a copy-to-clipboard button

--- pages/ezlogger/_rid.vue
+++ pages/ezlogger/_rid.vue
@@ -9,39 +9,54 @@
         </v-toolbar-title>
       </v-toolbar>
 
-      <v-list two-line>
-        <v-list-tile avatar>
-          <v-list-tile-avatar>
+      <v-card-text>
+        <v-text-field
+          id="url-input"
+          readonly
+          solo
+          single-line
+          full-width
+          hide-details
+          :suffix="request.method"
+          :value="request.url"
+        >
+          <template slot="prepend">
             <v-progress-circular v-if="request.status === 'processing'" indeterminate color="grey" />
-            <v-icon v-else-if="request.status === 'pending'" class="grey white--text">mdi-clock</v-icon>
-            <v-icon v-else-if="request.status === 'analyzed'" class="green white--text">mdi-lightbulb-on</v-icon>
-            <v-icon v-else-if="request.status === 'rejected'" class="orange white--text">mdi-lightbulb</v-icon>
-            <v-icon v-else-if="request.status === 'error'" class="red white--text">mdi-alert-circle-outline</v-icon>
-          </v-list-tile-avatar>
+            <v-icon v-else-if="request.status === 'pending'" class="grey--text">mdi-clock</v-icon>
+            <v-icon v-else-if="request.status === 'analyzed'" class="green--text">mdi-lightbulb-on</v-icon>
+            <v-icon v-else-if="request.status === 'rejected'" class="orange--text">mdi-lightbulb</v-icon>
+            <v-icon v-else-if="request.status === 'error'" class="red--text">mdi-alert-circle-outline</v-icon>
+          </template>
 
-          <v-list-tile-content>
-            <v-list-tile-title>{{ request.method }} {{ request.url }}</v-list-tile-title>
-            <v-list-tile-sub-title>
-              <v-tooltip bottom v-if="request.type">
-                <v-chip slot="activator" small label color="blue" text-color="white">{{ request.type }}</v-chip>
-                <span>{{ $t('ezLogger.requestType') }}</span>
-              </v-tooltip>
-              <v-tooltip bottom v-if="request.statusCode">
-                <v-chip slot="activator" small label color="blue" text-color="white">{{ request.statusCode }}</v-chip>
-                <span>{{ $t('ezLogger.statusCode') }}</span>
-              </v-tooltip>
-              <v-tooltip bottom v-if="request.ec && request.ec.rtype">
-                <v-chip slot="activator" small label color="green" text-color="white">{{ request.ec.rtype }}</v-chip>
-                <span>{{ $t('ezLogger.rtype') }}</span>
-              </v-tooltip>
-              <v-tooltip bottom v-if="request.ec && request.ec.mime">
-                <v-chip slot="activator" small label color="green" text-color="white">{{ request.ec.mime }}</v-chip>
-                <span>{{ $t('ezLogger.mime') }}</span>
-              </v-tooltip>
-            </v-list-tile-sub-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
+          <v-tooltip slot="append-outer" left>
+            <v-icon slot="activator" @click="copyUrl">mdi-clipboard</v-icon>
+            <span>{{ $t('ezLogger.copyToClipboard') }}</span>
+          </v-tooltip>
+        </v-text-field>
+      </v-card-text>
+
+      <v-card-text>
+        <v-tooltip bottom v-if="request.method">
+          <v-chip slot="activator" label color="blue" text-color="white">{{ request.method }}</v-chip>
+          <span>{{ $t('ezLogger.requestMethod') }}</span>
+        </v-tooltip>
+        <v-tooltip bottom v-if="request.type">
+          <v-chip slot="activator" label color="blue" text-color="white">{{ request.type }}</v-chip>
+          <span>{{ $t('ezLogger.requestType') }}</span>
+        </v-tooltip>
+        <v-tooltip bottom v-if="request.statusCode">
+          <v-chip slot="activator" label color="blue" text-color="white">{{ request.statusCode }}</v-chip>
+          <span>{{ $t('ezLogger.responseStatusCode') }}</span>
+        </v-tooltip>
+        <v-tooltip bottom v-if="request.ec && request.ec.rtype">
+          <v-chip slot="activator" label color="green" text-color="white">{{ request.ec.rtype }}</v-chip>
+          <span>{{ $t('ezLogger.rtype') }}</span>
+        </v-tooltip>
+        <v-tooltip bottom v-if="request.ec && request.ec.mime">
+          <v-chip slot="activator" label color="green" text-color="white">{{ request.ec.mime }}</v-chip>
+          <span>{{ $t('ezLogger.mime') }}</span>
+        </v-tooltip>
+      </v-card-text>
 
       <v-divider></v-divider>
 
@@ -113,6 +128,18 @@ export default {
         .filter(({ name, value }) => {
           return name.toLowerCase().includes(search) || value.toLowerCase().includes(search)
         })
+    }
+  },
+
+  methods: {
+    copyUrl () {
+      try {
+        document.getElementById('url-input').select()
+        document.execCommand('copy')
+      } catch (e) {
+        return this.$store.dispatch('snacks/error', 'ezLogger.urlCopyFailed')
+      }
+      this.$store.dispatch('snacks/success', 'ezLogger.urlCopySuccess')
     }
   }
 }
