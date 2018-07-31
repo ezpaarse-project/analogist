e1e332b02ff7136cc21ebcc30b8d12f89dfc647e
Steven WILMOUTH <wilmouthsteven@gmail.com>
2018-07-31 17:50:52 +0200

    Share badge, criteria

--- components/BadgeCard.vue
+++ components/BadgeCard.vue
@@ -6,13 +6,17 @@
           <v-card-media :src="badge.image" height="100%" contain></v-card-media>
         </v-flex>
         <v-flex xs7>
-          <div>
+          <div class="badge">
             <p class="headline" v-if="$i18n.locale === 'fr'">{{ badge.name }}</p>
             <p class="headline" v-else>{{ badge.alt_language[$i18n.locale].name }}</p>
             
-            <p v-if="$i18n.locale === 'fr'">{{ badge.description }}</p>
-            <p v-else>{{ badge.alt_language[$i18n.locale].description }}</p>
-            <p v-if="badge.issued_on">{{ $t('badges.issuedOn') }} : {{ issued_on }}</p>
+            <p v-if="$i18n.locale === 'fr'"><strong>Description</strong> : {{ badge.description }}</p>
+            <p v-else><strong>Description</strong> : {{ badge.alt_language[$i18n.locale].description }}</p>
+            
+            <p v-if="$i18n.locale === 'fr'"><strong>{{ $t('badges.criteria') }}</strong> : {{ badge.criteria }}</p>
+            <p v-else><strong>{{ $t('badges.criteria') }}</strong> : {{ badge.alt_language[$i18n.locale].criteria }}</p>
+
+            <p v-if="badge.issued_on"><strong>{{ $t('badges.issuedOn') }}</strong> : {{ issued_on }}</p>
           </div>
         </v-flex>
       </v-layout>
@@ -27,28 +31,30 @@
         <v-list class="sharing-list">
           <v-list-tile>
             <v-list-tile-content>
-              <a href="#/" target="_blank">
+              <a :href="`https://www.facebook.com/sharer/sharer.php?u=https://openbadgefactory.com/v1/badge/_/${badge.id}.png`" target="_blank">
                 <v-icon>mdi-facebook-box</v-icon> Facebook
               </a>
             </v-list-tile-content>
           </v-list-tile>
           <v-list-tile>
             <v-list-tile-content>
-              <a href="#/" target="_blank">
+              <a :href="`https://twitter.com/intent/tweet?size=medium&count=none&text=${badge.name}&url=https://openbadgefactory.com/v1/badge/_/${badge.id}.png&hashtags=ezPAARSE,AnalogIST`" target="_blank">
                 <v-icon>mdi-twitter-box</v-icon> Twitter
               </a>
             </v-list-tile-content>
           </v-list-tile>
           <v-list-tile>
             <v-list-tile-content>
-              <a href="#/" target="_blank">
+              <a :href="`https://plus.google.com/share?url=https://openbadgefactory.com/v1/badge/_/${badge.id}.png`" target="_blank">
                 <v-icon>mdi-google-plus-box</v-icon> Google+
               </a>
             </v-list-tile-content>
           </v-list-tile>
           <v-list-tile>
             <v-list-tile-content>
-              <a href="#/" target="_blank">
+              <a :href="`https://www.linkedin.com/shareArticle?mini=true&url=https://openbadgefactory.com/v1/badge/_/${badge.id}.png&title=${badge.name}&summary=AnalogIST%
+               ${badge.name}&source=AnalogIST
+`" target="_blank">
                 <v-icon>mdi-linkedin-box</v-icon> Linkedin
               </a>
             </v-list-tile-content>
@@ -89,5 +95,8 @@ export default {
 }
 .sharing-list a:hover {
   opacity: 0.8;
+}
+.badge p {
+  text-align: justify
 }
 </style>
