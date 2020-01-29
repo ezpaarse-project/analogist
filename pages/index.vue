5a2aa6247a26b5bc1ec610c83a65ae54f96fe18b
wilmouths <wilmouth.steven@gmail.com>
2020-01-29 15:04:42 +0100

    lang: update languages files

--- pages/index.vue
+++ pages/index.vue
@@ -49,15 +49,9 @@
 
           <v-layout row wrap mt-4>
             <v-flex xs12 sm12>
-              <p class="text-xs-justify">
-                <a href="https://www.ezpaarse.org/" target="_blank">ezPAARSE</a> est aujourd’hui livré avec une liste de <a href="https://github.com/ezpaarse-project/ezpaarse-platforms" target="_blank"><span class="font-weight-bold">{{ parsers }} parseurs</span></a>, qui sont le résultat d’un travail d’analyse fourni par la communauté, et qui assurent aux établissements utilisateurs une bonne couverture de leurs abonnements aux ressources électroniques.
-              </p>
-              <p class="text-xs-justify">
-                La plateforme communautaire, appelée <span class="font-weight-bold">AnalogIST</span>, est l’endroit où les différents types d'URLs sont collectés, analysés semi-automatiquement et commentés pour que le parseur correspondant puisse être précis et complet. Pour les éditeurs ou fournisseurs qui ne sont pas encore reconnus par <a href="https://www.ezpaarse.org/" target="_blank">ezPAARSE</a> ou qui ont simplement besoin d'une mise à jour, il est facile de venir contribuer à une analyse de plateforme : pour cela, il suffit d’ouvrir un compte Trello et de suivre le guide !
-              </p>
-              <p class="text-xs-justify">
-                Une fois l’analyse jugée suffisante, ou complète, un informaticien (généralement de l’équipe <a href="https://www.ezpaarse.org/" target="_blank">ezPAARSE</a>/<a href="https://ezmesure.couperin.org/" target="_blank">ezMESURE</a> prend le relais pour implémenter le parseur correspondant. Ce parseur viendra s’ajouter aux parseurs existants dans le dépôt <a href="https://github.com/ezpaarse-project/ezpaarse-platforms" target="_blank">github</a> dédié et pourra être récupéré par toutes les instances d’<a href="https://www.ezpaarse.org/" target="_blank">ezPAARSE</a>, par un mécanisme simple de mise à jour embarqué dans la zone d’administration du logiciel. Ce mode de fonctionnement garantit que chaque nouveau parseur, ainsi que chacune des mises à jour du logiciel et de ses ressources, profitent à toutes les installations existantes d’<a href="https://www.ezpaarse.org/" target="_blank">ezPAARSE</a> : le travail n’est nécessaire qu’à un endroit et une seule fois.
-              </p>
+              <p class="text-xs-justify" v-html="$t('home.whatIsEzPaarse', { parsers })"></p>
+              <p class="text-xs-justify" v-html="$t('home.whatIsAnalogist')"></p>
+              <p class="text-xs-justify" v-html="$t('home.goodAnalyses')"></p>
             </v-flex>
           </v-layout>
         </v-container>
